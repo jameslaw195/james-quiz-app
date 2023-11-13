448869780f0325bdb0450fd19877c992c5e9e882
James Lawrence <jameslaw195@gmail.com>
2023-11-13 14:02:43 +0100

    working on form page

--- js/form_script.js
+++ js/form_script.js
@@ -1,14 +1,30 @@
-const form = document.querySelector('[data-js="form"]')
-const submitBtn = document.querySelector('data-js="submit_button"]')
+const form = document.querySelector('[data-js="form"]');
+const submitBtn = document.querySelector('data-js="submit_button"]');
 
-submitBtn.addEventListener("input", (e) => {
-    e.preventDefault();
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    const formElements = e.target.elements
+  const formElements = e.target.elements;
+  const question = e.target.elements.form_question.value;
+  const answer = e.target.elements.form_answer.value;
+  const tags = e.target.elements.tags.value;
 
-})
+  const card = document.createElement("card");
+  document.body.append(card);
 
-submitBtn.addEventListener("click", (e)) {
-    const card = document.createElement("card");
-    document.body.append(card)
-}
+  card.innerHTML = `
+     <h2> ${question} </h2>
+     <p> ${answer} </p>
+     
+    `;
+});
+
+// submitBtn.addEventListener("click", (e)) {
+//     const card = document.createElement("card");
+//     document.body.append(card)
+
+//     card.innerHTML = `
+
+//     `
+
+// }
