e52e51ed95643dfc3923b72b069de0286258b6b1
James Lawrence <jameslaw195@gmail.com>
2023-11-13 11:26:31 +0100

    toggle bookmark

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
 const answerButton = document.querySelector('[data-js="answer_button"]');
 const answer = document.querySelector('[data-js="answer"]');
 
-bookmarkIcon.addEventListener("click", () => {
-  bookmarkIcon.setAttribute(src);
+bookmarkIcon.addEventListener("click", (e) => {
+  if (e.target.src.includes("filled.png")) {
+    bookmarkIcon.setAttribute("src", "images/bookmark_transparent.png");
+  } else {
+    bookmarkIcon.setAttribute("src", "images/bookmark_filled.png");
+  }
 });
 
 answerButton.addEventListener("click", () => {
@@ -14,3 +18,9 @@ answerButton.addEventListener("click", () => {
   }
   answer.toggleAttribute("hidden");
 });
+
+/*
+
+element.addEventListener("event name", function(e))
+
+*/
